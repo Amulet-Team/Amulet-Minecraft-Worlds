a80794f555602cefaeb4a0ab75740f9f3638b2f1
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-11-07 10:00:56 +0000

    Refactor

--- src/amulet/minecraft_worlds/level_data.py
+++ src/amulet/minecraft_worlds/level_data.py
@@ -15,21 +15,29 @@ class LevelData:
     dim_height: dict[str, tuple[tuple[int, int, int], tuple[int, int, int]]]
 
     def __init__(self, data: dict) -> None:
-        self._decode_world_data(data)
-        if data["world_data"]:
+        self._decode_data(data)
+
+    def _decode_data(self, data: dict) -> None:
+        world_data = dynamic_cast(data.pop("world_data"), dict)
+        self._decode_metadata(world_data)
+        if world_data:
             raise RuntimeError("Unhandled data in world_data")
-        del data["world_data"]
 
-        self._decode_dim_height(data)
+        dim_height = dynamic_cast(data.pop("dim_height"), dict)
+        self._decode_dim_height(dim_height)
 
-    def _decode_world_data(self, data: dict) -> None:
-        self.platform = dynamic_cast(data["world_data"]["platform"], str)
-        self.version = dynamic_cast(data["world_data"]["version"], str)
-        self.origin = dynamic_cast(data["world_data"]["origin"], str)
+        if data:
+            raise RuntimeError("Unhandled data in data")
 
-    def _decode_dim_height(self, data: dict) -> None:
+    def _decode_metadata(self, world_data: dict) -> None:
+        self.platform = dynamic_cast(world_data.pop("platform"), str)
+        self.version = dynamic_cast(world_data.pop("version"), str)
+        self.origin = dynamic_cast(world_data.pop("origin"), str)
+
+
+    def _decode_dim_height(self, dim_height: dict) -> None:
         self.dim_height = {}
-        for k, ((min_x, min_y, min_z), (max_x, max_y, max_z)) in dynamic_cast(data["dim_height"], dict).items():
+        for k, ((min_x, min_y, min_z), (max_x, max_y, max_z)) in dim_height.items():
             self.dim_height[dynamic_cast(k, str)] = (
                 (
                     dynamic_cast(min_x, int),
